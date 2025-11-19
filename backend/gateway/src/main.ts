f71765dc1f1aee6ff8b5b73b39c6a630fcbd695e
hampachovsky <hampe1324@gmail.com>
2025-11-19 10:55:55 +0200

    add cookie-parser

--- backend/gateway/src/main.ts
+++ backend/gateway/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('started gateway 3000');
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
   app.enableCors({ credentials: true, origin: true });
   await app.listen(process.env.PORT ?? 3000);
 }
